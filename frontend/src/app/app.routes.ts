7053c014ed7c5a782702f2f395dfb47ec012cf04
abanoub78 <beboyosry5@gmail.com>
2025-10-28 15:44:23 +0300

    Other authentication pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,18 @@ import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { CompleteProfile } from './components/complete-profile/complete-profile';
 import { AdminDashboard } from './components/admin-dashboard/admin-dashboard';
+import { Notfound } from './components/notfound/notfound';
+import { ForgotPassword } from './components/forgot-password/forgot-password';
+import { ResetPassword } from './components/reset-password/reset-password';
+
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'complete-profile', component: CompleteProfile },
   { path: 'dashbord', component: AdminDashboard },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'reset-password', component: ResetPassword },
 
-  { path: '**', redirectTo: '' },
+  { path: '**', component: Notfound },
 ];
