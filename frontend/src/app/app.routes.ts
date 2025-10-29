36e60a661010d2e6e3bb285d98b871e0a226f833
SarahMahmouddd <Sarah.20367686@compit.aun.edu.eg>
2025-10-29 13:19:10 +0300

    profile page component

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,12 +4,14 @@ import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { CompleteProfile } from './components/complete-profile/complete-profile';
 import { AdminDashboard } from './components/admin-dashboard/admin-dashboard';
+import { ProfilePage } from './components/profile-page/profile-page';
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'complete-profile', component: CompleteProfile },
   { path: 'dashbord', component: AdminDashboard },
+  { path: 'profile-page', component: ProfilePage },
 
   { path: '**', redirectTo: '' },
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
+import { Home } from './components/home/home';
+import { ProfilePage } from './components/profile-page/profile-page';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Home, ProfilePage],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
