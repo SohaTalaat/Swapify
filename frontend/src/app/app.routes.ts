d7d782ad7ab8904d7533ff10814ef3b2af9be080
SarahMahmouddd <Sarah.20367686@compit.aun.edu.eg>
2025-10-29 13:54:16 +0300

    profile

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -4,23 +4,18 @@ import { Login } from './components/login/login';
 import { Register } from './components/register/register';
 import { CompleteProfile } from './components/complete-profile/complete-profile';
 import { AdminDashboard } from './components/admin-dashboard/admin-dashboard';
-import { ProfilePage } from './components/profile-page/profile-page';
+import { Notfound } from './components/notfound/notfound';
+import { ForgotPassword } from './components/forgot-password/forgot-password';
+import { ResetPassword } from './components/reset-password/reset-password';
+
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
   { path: 'complete-profile', component: CompleteProfile },
   { path: 'dashbord', component: AdminDashboard },
-
-
-
-
-
-
-
-
-
-  { path: 'profile-page', component: ProfilePage },
-
-  { path: '**', redirectTo: '' },
+  { path: 'forgot-password', component: ForgotPassword },
+  { path: 'reset-password', component: ResetPassword },
+{path: 'profile-page' , component: ProfilePage}
+  { path: '**', component: Notfound },
 ];
