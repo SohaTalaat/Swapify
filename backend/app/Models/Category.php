a2fa03993c4b9510714c82e8b54b3a6d90c63011
Soha <saha.20350436@science.aun.edu.eg>
2025-11-03 22:32:28 +0200

    Fix some endpoints and models

--- backend/app/Models/Category.php
+++ backend/app/Models/Category.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = [
+        'name',
+        'type',
+        'description'
+    ];
     public function listings()
     {
         return $this->hasMany(Listing::class);
