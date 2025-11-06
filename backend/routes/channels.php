4e2cbc1f64928f4d37d02b84a6b339dc6ff92fba
Abdelrahman Ramadan <ramadanabdelrahman29@gmail.com>
2025-11-06 14:25:32 +0200

    Updated MessageController to trigger NewMessageSent event and Added Pusher credentials to .env

---
+++ backend/routes/channels.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Broadcast;
+
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Here you may register all of the event broadcasting channels that your
+| application supports. The given channel authorization callbacks are
+| used to check if an authenticated user can listen to the channel.
+|
+*/
+
+Broadcast::channel('chat.{chatId}', function ($user, $chatId) {
+    
+    return $user->barters()->whereHas('chat', function ($q) use ($chatId) {
+        $q->where('id', $chatId);
+    })->exists();
+});
+
