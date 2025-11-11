355d1c0bef4cf51b388a960509d18a62afbf9097
abanoub78 <beboyosry5@gmail.com>
2025-11-11 16:28:21 +0200

    test

--- frontend/src/app/services/echo.ts
+++ frontend/src/app/services/echo.ts
@@ -23,8 +23,8 @@ export class EchoService {
 
     this.echo = new Echo({
       broadcaster: 'pusher',
-      key: '3ad51a0a95a1b73945f5',
-      cluster: 'eu',
+      key: 'ee780b9d138923a2bcf4',
+      cluster: 'mt1',
       forceTLS: true,
       authEndpoint: 'http://127.0.0.1:8000/broadcasting/auth',
       auth: {
